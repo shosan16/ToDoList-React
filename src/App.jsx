6afa4e3a31718ffb2866e5b05fa6938bc18fc725
Shota Nagayama <30305829kasukabe@gmail.com>
2022-10-15 00:01:04 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,18 @@ export const App = () => {
   ]);
   const [completeTodos, setcompleteTodos] = useState(["うううう", "ええええ"]);
 
-
-
   return (
     <>
-      <div class="input-area">
+      <div className="input-area">
         <input id="add-text" placeholder="To Doを入力"></input>
         <button id="add-button">追加</button>
       </div>
-      <div class="incomplete-area">
-        <p class="title">未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul id="incomplete-list">
           {incompleteTodos.map((todo) => {
             return (
-              <div key={todo} class="list-row">
+              <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
                 <button>削除</button>
@@ -31,12 +29,12 @@ export const App = () => {
           })}
         </ul>
       </div>
-      <div class="complete-area">
-        <p class="title">完了したTODO</p>
+      <div className="complete-area">
+        <p className="title">完了したTODO</p>
         <ul id="complete-list">
           {completeTodos.map((todo) => {
             return (
-              <div class="list-row">
+              <div className="list-row">
                 <li>{todo}</li>
                 <button>戻す</button>
               </div>
